7fbbbf4783a5db8b2668405f44a2b1999c5aed1f
Андрей Лейкин <katashi1328@mail.ru>
2018-06-06 13:13:03 +0300

    first commit

--- build/app/js/app.js
+++ build/app/js/app.js
@@ -1,7 +1,23 @@
 // Начальная функция
 
 (function(){
-	console.log('app');
+	$(".navigation").waypoint(function() {
+		$(this.element).addClass("viewed");
+	},{offset: '100%'});
+	$(".doing").waypoint(function() {
+		$(this.element).addClass("viewed");
+	},{offset: '100%'});
+
+	function scrollNav() {
+		$('.js-scroll-to').on("click", function(){  
+			//Animate
+			$('html, body').stop().animate({
+					scrollTop: $( $(this).attr('href') ).offset().top - 100
+			}, 400);
+			return false;
+		});
+	}
+	scrollNav();
 })();
 $(document).ready(function () {
     svg4everybody({});
--- dev/static/js/app.js
+++ dev/static/js/app.js
@@ -1,5 +1,21 @@
 // Начальная функция
 
 (function(){
-	console.log('app');
+	$(".navigation").waypoint(function() {
+		$(this.element).addClass("viewed");
+	},{offset: '100%'});
+	$(".doing").waypoint(function() {
+		$(this.element).addClass("viewed");
+	},{offset: '100%'});
+
+	function scrollNav() {
+		$('.js-scroll-to').on("click", function(){  
+			//Animate
+			$('html, body').stop().animate({
+					scrollTop: $( $(this).attr('href') ).offset().top - 100
+			}, 400);
+			return false;
+		});
+	}
+	scrollNav();
 })();
